c0e8aaf3bf8db9bf0f5771ffde045e997c8b724d
Dmitry <dmitry.r@cyberflow.net>
2016-07-11 10:43:41 +0300

    fix rubocop

--- test/fixtures/cookbooks/elasticsearch-curator-test/recipes/default.rb
+++ test/fixtures/cookbooks/elasticsearch-curator-test/recipes/default.rb
@@ -1,26 +1,26 @@
 c = {
-    'actions' => {
-      1 => {
-        'action' => 'delete_indices',
-        'description' => 'Delete indices older than 7',
-        'options' => {
-          'ignore_empty_list' => true,
-          'continue_if_exception' => true,
-          'disable_action' => false
-        },
-        'filters' => [{
-          'filtertype' => 'pattern',
+  'actions' => {
+    1 => {
+      'action' => 'delete_indices',
+      'description' => 'Delete indices older than 7',
+      'options' => {
+        'ignore_empty_list' => true,
+        'continue_if_exception' => true,
+        'disable_action' => false
+      },
+      'filters' => [
+        { 'filtertype' => 'pattern',
           'kind' => 'prefix',
-          'value' => 'logstash-'},
-          {'filtertype' => 'age',
+          'value' => 'logstash-' },
+        { 'filtertype' => 'age',
           'source' => 'creation_date',
           'direction' => 'older',
           'unit' => 'days',
-          'unit_count' => 7}
-        ]
-      }
+          'unit_count' => 7 }
+      ]
     }
   }
+}
 
 elasticsearch_curator_action 'action' do
   config c
