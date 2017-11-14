2e58a93c4e5f1af71327b8b3ef9f13b2ef44c9da
Maxim Darii <mdarii@tacitknowledge.com>
2017-11-14 09:39:44 +0200

    Added support for centos 6

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,12 @@ case node['platform_family']
 when 'debian'
   default['elasticsearch-curator']['repository_url'] = 'http://packages.elastic.co/curator/4/debian'
 when 'rhel'
-  default['elasticsearch-curator']['repository_url'] = 'http://packages.elastic.co/curator/4/centos/7'
+  default['elasticsearch-curator']['repository_url'] = case node['platform_version'].to_i
+    when 6
+        'http://packages.elastic.co/curator/4/centos/6'
+    when 7
+        'http://packages.elastic.co/curator/4/centos/7'
+    end
 end
 default['elasticsearch-curator']['repository_key'] = 'https://packages.elastic.co/GPG-KEY-elasticsearch'
 default['elasticsearch-curator']['bin_path'] = '/usr/bin'
@@ -27,3 +32,4 @@ default['elasticsearch-curator']['config_file_path'] = "/home/#{node['elasticsea
 default['elasticsearch-curator']['action_file_path'] = "/home/#{node['elasticsearch-curator']['username']}/.curator"
 default['elasticsearch-curator']['cron_minute'] = '0'
 default['elasticsearch-curator']['cron_hour'] = '*'
+override['poise-python']['options']['wheel_version'] = '0.29.0' if node['platform_version'].to_i == 6
